de73e0048a74ce5ddac9291db39a08d6f805dc52
sun mengge <mengge.sun@qianli-inc.com>
2019-09-17 19:03:11 +0800

    HOC demo

--- src/lib/decorators.js
+++ src/lib/decorators.js
@@ -1,8 +1,28 @@
 import Immutable from 'immutable';
 
-export function shouldComponentUpdate(target) {
-  target.prototype.shouldComponentUpdate = function (nextProps, nextState) {
-    return !Immutable.is(Immutable.fromJS(nextProps), Immutable.fromJS(this.props)) ||
-      !Immutable.is(Immutable.fromJS(nextState), Immutable.fromJS(this.state))
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+export function shouldComponentUpdate(OriginalComponent) {
+  return class EnhancedComponent extends OriginalComponent {
+    constructor(props) {
+      super(props);
+      this.shouldUpdate = OriginalComponent.prototype.shouldComponentUpdate;
+    }
+
+    static displayName = `HOC(${getDisplayName(OriginalComponent)})`;
+
+    shouldComponentUpdate(nextProps, nextState) {
+      if (Object.prototype.toString.call(this.shouldUpdate) === '[object Function]') {
+        this.shouldUpdate(nextProps, nextState);
+      }
+      const result = !Immutable.is(Immutable.fromJS(nextProps), Immutable.fromJS(this.props)) ||
+        !Immutable.is(Immutable.fromJS(nextState), Immutable.fromJS(this.state));
+      console.log(nextProps, nextState);
+      console.log(this.props, this.state);
+      console.log(result);
+      return result;
+    }
   }
 }
--- src/routes/refs-demo/components/InputHOC.js
+++ src/routes/refs-demo/components/InputHOC.js
@@ -1,24 +1,37 @@
 import React from 'react';
 
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
 export default function InputHOC(WrappedComponent) {
-  class LogProps extends React.Component {
+  class WrapperComponent extends React.Component {
+    static displayName = `HOC(${getDisplayName(WrappedComponent)})`;
+
     componentDidUpdate(prevProps) {
       console.log('old props:', prevProps);
       console.log('new props:', this.props);
     }
 
     render() {
-      const {forwardedRef, ...rest} = this.props;
+      const {forwardRef, ...rest} = this.props;
 
       // 将自定义的 prop 属性 “forwardedRef” 定义为 ref
-      return <WrappedComponent forwardRef={forwardedRef} {...rest} />;
+      return <WrappedComponent forwardRef={forwardRef} {...rest} />;
     }
   }
+
+  // class WrapperComponent extends WrappedComponent {
+  //   componentDidUpdate(prevProps) {
+  //     console.log('old props:', prevProps);
+  //     console.log('new props:', this.props);
+  //   }
+  // }
 
   // 注意 React.forwardRef 回调的第二个参数 “ref”。
   // 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
   // 然后它就可以被挂载到被 LogPros 包裹的子组件上。
   return React.forwardRef((props, ref) => {
-    return <LogProps {...props} forwardedRef={ref} />;
+    return <WrapperComponent {...props} forwardRef={ref} />;
   });
 }
