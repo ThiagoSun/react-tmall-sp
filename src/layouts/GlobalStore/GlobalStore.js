39232d341e3d68831b6d67d96237821a0133cf13
Sun Mengge <sunmengge@xiaoheiban.cn>
2021-07-19 11:01:39 +0800

    feat: 修改withRouter引用

--- src/layouts/GlobalStore/GlobalStore.js
+++ src/layouts/GlobalStore/GlobalStore.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Toast} from 'antd-mobile';
-import { withRouter } from "react-router";
+import { withRouter } from "react-router-dom";
 
 const Fragment = React.Fragment;
 
